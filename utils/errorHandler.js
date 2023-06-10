7d76554dd592b0dc3ebeea47b010c274d0ddab89
makaubenson <bensonmakau2000@gmail.com>
2023-06-10 16:39:22 +0300

    updated readme

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,6 +1,8 @@
 exports.getErrorMessage = function (err, message) {
   if (process.env.NODE_ENV === "development") {
     return err.message;
+  } else if (process.env.NODE_ENV === "production") {
+    return message;
   }
   return message;
 };
